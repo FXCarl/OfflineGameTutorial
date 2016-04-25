548a5a7328a98f389fd3f04c16aa836f29bdb169
FXCarl <fxcarlcn@gmail.com>
2016-04-25 23:11:38 +0800

    now support HFSM

--- Assets/Codes/HierarchyStateMachine.cs
+++ Assets/Codes/HierarchyStateMachine.cs
@@ -23,8 +23,15 @@ public class HierarchyStateMachine : MonoBehaviour, IHierarchyStateMachine{
 
     public void SetState(int index)
     {
-        if (transform.childCount < 1) return;
-        if (index < 0 || index >= transform.childCount) return;
+        if (index >= transform.childCount && transform.parent != null)
+        {
+            ExecuteEvents.ExecuteHierarchy<IHierarchyStateMachine>(transform.parent.gameObject, null, (machine, data) => machine.SetNextState());
+        }
+
+        if (transform.childCount < 1 || index < 0 || index >= transform.childCount)
+        {
+            return;
+        }
 
         Transform trs;
         for (int i = 0; i< transform.childCount; i++)
