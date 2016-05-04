e5815fc50d6219889cf009d3e1dd369d1facdb6b
liweikuan <497601901@qq.com>
2016-05-04 10:36:39 +0800

    TutorialOfflineDocumentFolder_Add
    离线新手project 文档目录创建

--- Assets/Codes/BindingInputControlScript.cs
+++ Assets/Codes/BindingInputControlScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BindingInputControlScript : MonoBehaviour {
 
     public GameObject[] obj;
+    public GameObject[] Act;
 	// Use this for initialization
 	void Start () {
         obj = new GameObject[transform.childCount];
@@ -14,9 +15,22 @@ public class BindingInputControlScript : MonoBehaviour {
             obj[i] = transform.GetChild(i).gameObject;
             obj[i].AddComponent<InputControlNextState>(); 
             Debug.Log(obj[i].name);
+            BindingAudioSources(obj[i]);
         }
     }
-	
+
+    private void BindingAudioSources(GameObject _obj) {
+        if (_obj.ToString().StartsWith("Act"))
+        {
+            Act = new GameObject[_obj.transform.childCount];
+            for (int i = 0; i < _obj.transform.childCount; i++)
+            {
+                Act[i] = _obj.transform.GetChild(i).gameObject;
+                Act[i].AddComponent<AudioSource>();
+            }
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 	
