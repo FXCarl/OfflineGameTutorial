9fcfbf471a1b06936ddbb00f451c98a8ba206e35
wengyiren <43675512@qq.com>
2016-05-06 11:47:31 +0800

    add new avatar

--- Assets/Avatar/Scripts/Avatar/Avatar.cs
+++ Assets/Avatar/Scripts/Avatar/Avatar.cs
@@ -38,6 +38,9 @@ namespace Mooji.Avatar
         protected GameObject idelAnimGo;
 
         [SerializeField]
+        protected GameObject deadAnimGo;
+
+        [SerializeField]
         protected AnimDir currAnimDir;
 
         [SerializeField]
