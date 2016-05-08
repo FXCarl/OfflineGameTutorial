9ececc2080258ca9a6d4341d39afcbf480c07c43
wengyiren <43675512@qq.com>
2016-05-09 03:49:05 +0800

    u

---
+++ Assets/ImgHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImgHandler : MonoBehaviour {
+
+    public Transform t;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void OnClickedHandler()
+    {
+        t.gameObject.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+}
